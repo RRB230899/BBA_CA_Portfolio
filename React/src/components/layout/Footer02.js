d568f689b3f3dba727630f02411f2a5f59c1336c
Raghav Bajaj <bajajraghav9@gmail.com>
2025-04-01 02:27:55 -0400

    Oopsie...

--- React/src/components/layout/Footer02.js
+++ React/src/components/layout/Footer02.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Logo from './partials/LogoInvert';
-import FooterNav from './partials/FooterNav';
+// import FooterNav from './partials/FooterNav';
 
 const propTypes = {
   topOuterDivider: PropTypes.bool,
